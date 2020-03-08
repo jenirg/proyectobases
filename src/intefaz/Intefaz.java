727130b1cf21ad0e43eca9f1ce23732ec4f9d6bd
AGMH16 <42829134+AGMH16@users.noreply.github.com>
2020-03-08 00:06:48 -0600

    Subimos el proyecto Interfaz a gitHub

---
+++ src/intefaz/Intefaz.java
@@ -0,0 +1,20 @@
+
+package intefaz;
+
+/**
+ *
+ * @author dell
+ */
+public class Intefaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i=1950; i<=2020;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
